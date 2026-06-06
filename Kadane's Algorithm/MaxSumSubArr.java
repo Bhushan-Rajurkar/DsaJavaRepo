8d2376423d1adf30ceb3c471326669d65b1f2985
Bhushan Raju Rajurkar <bhushanrajurajurkar@gmail.com>
2026-06-06 22:12:02 +0530

    Implement Kadane's Algorithm for max subarray sum
    for leetcode  problem 53

--- Kadane's Algorithm/MaxSumSubArr.java
+++ Kadane's Algorithm/MaxSumSubArr.java
@@ -1,1 +1,21 @@
-
+class MaxSumSubArr {
+  public static void main(String args[]){
+    int[] nums={-2,1,-3,4,-1,2,1,-5,4};
+    MaxSumSubArr obj = new MaxSumSubArr();
+        System.out.println(obj.maxSubArray(nums));
+  }
+    public int maxSubArray(int[] nums) {
+          int n=nums.length;
+          int i=0;
+          int bestend=nums[0];
+          int sum=nums[0];
+          for(i=1;i<n ;i++){
+            int v1=bestend+nums[i];
+            int v2= nums[i];
+            bestend= Math.max(v1,v2);
+            sum= Math.max(sum,bestend); 
+            } 
+            return sum;
+        
+    }
+}
